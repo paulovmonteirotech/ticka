cc7a6946dfa5db3e2a04031a3027f61625a5cfcf
paulovmonteirotech <161640491+paulovmonteirotech@users.noreply.github.com>
2025-11-28 13:19:43 -0300

    Comentarios
    Comentários e aperfeiçoamento do new_user

--- mailup.php
+++ mailup.php
@@ -1,28 +1,34 @@
 <?php
-
-
+// Biblioteca PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require './PHPMailer/src/Exception.php';
 require './PHPMailer/src/PHPMailer.php';
 require './PHPMailer/src/SMTP.php';
+//___________________________________________________//
+
 
 header("Content-type: text/html; charset=utf-8");
 
+// Verifica se tipo foi passado pelo método POST
 if (!isset($_POST['tipo'])) {
     die("Tipo de chamado não especificado.");
 }
 
+//Atribui o valor de tipo // importante para o switch case
 $tipo = $_POST['tipo'];
+//___________________________________________________//
 
-// Dados básicos
+// Variaveis que nao são arrays
 $setor      = $_POST['setor'] ?? '';
 $requerente = $_POST['requerente'] ?? '';
 $descricao  = $_POST['descricao'] ?? '';
 $email      = $_POST['email'] ?? '';
 $funcionario      = $_POST['funcionario'] ?? '';
-
+$outro_servico      = $_POST['outro_servico'] ?? '';
+$copy_user      = $_POST['copy_user'] ?? '';
+//___________________________________________________//
 
 // ===Processa os Checkboxes ===
 // Verifica se o array 'problemas' existe e não está vazio
@@ -32,14 +38,25 @@ if (isset($_POST['problemas']) && is_array($_POST['problemas'])) {
 } else {
     $problemas_selecionados = 'Nenhum problema selecionado ou problema não especificado.';
 }
-// ===================================
+
+// Verifica se o array 'servicos' existe e não está vazio
+if (isset($_POST['servicos']) && is_array($_POST['servicos'])) {
+    // Junta os problemas selecionados em uma string separada por vírgulas e espaços.
+    $servicos_selecionados = implode(', ', $_POST['servicos']);
+} else {
+    $servicos_selecionados = 'Nenhum servico selecionado.';
+}
+//___________________________________________________//
+
+// Recurso Técnico Emergencial (RTE)
+// Verifica se o email do requerente existe, se não, poe o um email válido 
 if (isset($_POST['email']) && is_string($_POST['problemas'])) {
     
     $email = ($_POST['email']);
 } else {
     $email = 'exemple@exemplo.com';
 }
-
+//___________________________________________________//
 // ============================
 // 🔵 SISTEMA DE ROTAS
 // ============================
@@ -84,10 +101,16 @@ switch ($tipo) {
         $assunto = "Chamado Novo Usuário - $requerente";
         $mensagem = "
             <h2>Novo Usuário</h2>
+            <b>Novo Funcionário:</b> $funcionario<br>
             <b>Setor:</b> $setor<br>
+            <b>Tipo de funcionario:</b> $problemas_selecionados<br>
+            <b>Outro tipo:</b> $descricao<br>
+            <b>Serviços:</b> $servicos_selecionados<br>
+            <b>Outros $outro_servico<br>
+            <b>Copiar Usuário:</b> $copy_user<br>
             <b>Requerente:</b> $requerente<br>
-            <b>Problema(s):</b> $problemas_selecionados<br>
-            <b>Observações:</b> $descricao<br>
+            <br>
+            
             <b>E-mail:</b> $email<br>
         ";
     break;
@@ -96,11 +119,12 @@ switch ($tipo) {
     default:
         die("Tipo de chamado inválido.");
 }
-
+//___________________________________________________//
 
 // ============================
 // 🔵 Envio do e-mail (PHPMailer)
 // ============================
+
 
 $mail = new PHPMailer(true);
 
@@ -119,8 +143,8 @@ try {
     // De e para
     $mail->setFrom('exemple@exemplo.com', 'Chamados Ticka');
     $mail->addAddress('exemple@exemplo.com'); // para destino do suporte
-    $mail->addAddress($email);
-    $mail->addReplyTo('exemple@exemplo.com'); // para destino do requerente
+    $mail->addAddress($email);// para destino do requerente
+    $mail->addReplyTo('exemple@exemplo.com'); // Replay To
 
     // Conteúdo
     $mail->isHTML(true);
